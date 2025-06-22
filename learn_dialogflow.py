5200b75824c43d76db283b079548d90116e952f3
Lek <ligioner29@mail.ru>
2025-06-22 15:11:22 +0800

    add DialogFlow learning script

---
+++ learn_dialogflow.py
@@ -0,0 +1,71 @@
+import os
+import json
+from environs import Env
+from google.cloud import dialogflow_v2 as dialogflow
+
+env = Env()
+env.read_env()
+
+PROJECT_ID = env.str("PROJECT_ID")
+
+credentials_file_name = env.str("GOOGLE_APPLICATION_CREDENTIALS")
+credentials_path = os.path.join(os.getcwd(), credentials_file_name)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+PARENT_PATH = dialogflow.AgentsClient.agent_path(PROJECT_ID)
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part],
+                                                           type_=dialogflow.Intent.TrainingPhrase.Type.EXAMPLE)
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message],
+    )
+
+    try:
+        response = intents_client.create_intent(
+            request={"parent": PARENT_PATH, "intent": intent}
+        )
+        print(f"Интент '{response.display_name}' успешно создан. ID: {response.name}")
+    except Exception as e:
+        print(f"Ошибка при создании интента '{display_name}': {e}")
+
+
+
+if __name__ == '__main__':
+    try:
+        with open('questions.json', 'r', encoding='utf-8') as f:
+            questions_data = json.load(f)
+    except FileNotFoundError:
+        print("Ошибка: Файл 'questions.json' не найден.")
+        exit()
+    except json.JSONDecodeError:
+        print("Ошибка: Файл 'questions.json' содержит некорректный формат JSON.")
+        exit()
+
+    print("Начинаем создание интентов в Dialogflow...")
+
+
+    for intent_name, data in questions_data.items():
+        print(f"\nСоздаем интент: '{intent_name}'")
+        create_intent(
+            PROJECT_ID,
+            intent_name,
+            data['questions'],
+            [data['answer']]
+        )
+
+    print("\nПроцесс создания интентов завершен.")
+
