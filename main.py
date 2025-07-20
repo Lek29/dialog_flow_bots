cc163641dd4c3efbe50f000cf13e359bc97ac6db
Lek <ligioner29@mail.ru>
2025-07-21 06:15:28 +0800

    изменил  вызов логгера в main и добавил sys.exception

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import logging
 import sys
 import threading
-import time
 import traceback
 
 from telegram_notifier import send_dev_alert
 from tg_bot import run_tg_bot
 from vk_bot import run_vk_bot
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+
+# logging.basicConfig(
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#     level=logging.INFO
+# )
 logger = logging.getLogger(__name__)
 
 
@@ -28,6 +28,13 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 
 
 def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
+    sys.excepthook = handle_exception
+
     logger.info('Запуск ботов...')
     telegram_thread = threading.Thread(target=run_tg_bot, daemon=True)
     vk_thread = threading.Thread(target=run_vk_bot, daemon=True)
