f22828c221e9f3ba093124071a68f27b59b0fd9a
Lek <ligioner29@mail.ru>
2025-08-02 06:12:51 +0800

    убрал глобальные переменные из telegram_notiffier

--- telegram_notifier.py
+++ telegram_notifier.py
@@ -5,12 +5,8 @@ from telegram import Bot
 
 logger = logging.getLogger(__name__)
 
-_get_notifier_resources = None
 
-def _initialize_once():
-    """
-        Выполняет всю инициализацию и возвращает кортеж (бот, chat_id).
-    """
+def get_notifier_bot_and_chat_id():
 
     env = Env()
     env.read_env()
@@ -36,21 +32,12 @@ def _initialize_once():
         return None, None
 
 def send_dev_alert(message: str):
-    global _get_notifier_resources
 
-    if _get_notifier_resources is None:
-        bot, chat_id = _initialize_once()
+    bot, chat_id = get_notifier_bot_and_chat_id()
 
-        def _get_resources():
-            return bot, chat_id
-
-        _get_notifier_resources = _get_resources
-
-    notifier_bot, developer_chat_id = _get_notifier_resources()
-
-    if notifier_bot and developer_chat_id:
+    if bot and chat_id:
         try:
-            notifier_bot.send_message(chat_id=developer_chat_id, text=message)
+            bot.send_message(chat_id=chat_id, text=message)
             logger.info(f'Отправлено уведомление в Telegram: {message[:100]}...')
         except Exception as e:
             logger.error(f'Не удалось отправить уведомление: {e}')
@@ -60,4 +47,9 @@ def send_dev_alert(message: str):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
     send_dev_alert("Тестовое уведомление")
