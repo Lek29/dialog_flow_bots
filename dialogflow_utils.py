bec4372dc17c8d1c1dfef3e8fca1162bd943cc0e
Lek <ligioner29@mail.ru>
2025-08-06 15:51:33 +0800

    изменил получение токенов в tg_bot

--- dialogflow_utils.py
+++ dialogflow_utils.py
@@ -4,32 +4,16 @@ from environs import Env
 from google.cloud import dialogflow_v2 as dialogflow
 
 
-def detect_intent_texts(session_id, text, language_code='ru-RU'):
+def detect_intent_texts(project_id, credentials_path, session_id, text, language_code='ru-RU'):
     """
     Отправляет текстовый запрос в Dialogflow и возвращает ответ.
-    Использование одного и того же session_id позволяет продолжать диалог (контекст).
 
-    Args:
-        session_id (str): Уникальный идентификатор сессии для диалога. Обычно это ID пользователя.
-        text (str): Текст сообщения пользователя.
-        language_code (str): Код языка (например, 'ru-RU' для русского).
-
-    Returns:
-         google.cloud.dialogflow_v2.types.QueryResult: Объект QueryResult из Dialogflow,
-                                                       или None, если произошла ошибка API.
     """
-
-    env = Env()
-    env.read_env()
-
-    PROJECT_ID = env.str('PROJECT_ID')
-    credentials_file_name = env.str('GOOGLE_APPLICATION_CREDENTIALS')
-    credentials_path = os.path.join(os.getcwd(), credentials_file_name)
 
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 
     session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(PROJECT_ID, session_id)
+    session = session_client.session_path(project_id, session_id)
 
     text_input = dialogflow.TextInput(text=text, language_code=language_code)
     query_input = dialogflow.QueryInput(text=text_input)
@@ -41,4 +25,10 @@ def detect_intent_texts(session_id, text, language_code='ru-RU'):
 
 
 if __name__ == '__main__':
-    detect_intent_texts()
+    env = Env()
+    env.read_env()
+    project_id = env.str('PROJECT_ID')
+    credentials_file_name = env.str('GOOGLE_APPLICATION_CREDENTIALS')
+    credentials_path = os.path.join(os.getcwd(), credentials_file_name)
+
+    detect_intent_texts(project_id, credentials_path, 'test_session', 'Привет')
--- tg_bot.py
+++ tg_bot.py
@@ -1,12 +1,15 @@
+import os
 import logging
+from functools import partial
 
 from environs import Env
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, Updater)
 
 from dialogflow_utils import detect_intent_texts
 from telegram_notifier import send_dev_alert
+
 
 logger = logging.getLogger(__name__)
 
@@ -19,13 +22,18 @@ def start(update: Update, context: CallbackContext) -> None:
     logger.info(f'Telegram-бот: Пользователь {user.id} начал диалог.')
 
 
-def handle_message(update: Update, context: CallbackContext) -> None:
+def handle_message(update: Update, context: CallbackContext, dialogflow_settings: dict, notifier_settings: dict) -> None:
     user_id = update.effective_user.id
     user_text = update.message.text
     logger.info(f'Telegram-бот: Новое сообщение от {user_id}: "{user_text}"')
+    notifier_bot = notifier_settings['bot']
+    notifier_chat_id = notifier_settings['chat_id']
+    project_id = dialogflow_settings['project_id']
+    credentials_path = dialogflow_settings['credentials_path']
+
 
     try:
-        query_result = detect_intent_texts(f'tg-{user_id}', user_text, 'ru')
+        query_result = detect_intent_texts(project_id, credentials_path, f'tg-{user_id}', user_text, 'ru')
 
         if query_result.intent.is_fallback:
             dialogflow_response_text = 'Извините, я не понял ваш запрос. Пожалуйста, попробуйте еще раз.'
@@ -40,7 +48,10 @@ def handle_message(update: Update, context: CallbackContext) -> None:
     except Exception as e:
         logger.critical(f'Telegram-бот: Ошибка Dialogflow или обработки сообщения для {user_id}: {e}', exc_info=True)
         send_dev_alert(
-            f'Telegram-бот: Критическая ошибка Dialogflow!\n\nОт пользователя {user_id}: "{user_text}"\n\n```\n{e}\n```')
+            f'Telegram-бот: Критическая ошибка Dialogflow!\n\nОт пользователя {user_id}: "{user_text}"\n\n```\n{e}\n```',
+            notifier_bot,
+            notifier_chat_id
+        )
         update.message.reply_text('Произошла внутренняя ошибка. Пожалуйста, попробуйте позже.')
 
 
@@ -54,21 +65,40 @@ def run_tg_bot():
     env.read_env()
 
     telegram_token = env.str('BOT_TOKEN')
+    project_id = env.str('PROJECT_ID')
+    credentials_file_name = env.str('GOOGLE_APPLICATION_CREDENTIALS')
+    developer_chat_id = env.str('DEVELOPER_CHAT_ID')
 
     if not telegram_token:
         logger.error('telegram_token не установлен в .env. Telegram-бот не будет работать.')
-
-        send_dev_alert('Telegram-бот: telegram_token не установлен в .env. Бот не запущен.')
+        try:
+            temp_bot = Bot(telegram_token)
+            send_dev_alert('Telegram-бот: telegram_token не установлен в .env. Бот не запущен.',
+                           temp_bot, developer_chat_id)
+        except Exception:
+            pass
         return
+
+    try:
+        notifier_bot = Bot(token=telegram_token)
+        notifier_settings = {'bot': notifier_bot, 'chat_id': developer_chat_id}
+    except Exception as e:
+        logger.error(f"Ошибка при создании Notifier Bot: {e}")
+        return
+
+    credentials_path = os.path.join(os.getcwd(), credentials_file_name)
+    dialogflow_settings = {'project_id': project_id, 'credentials_path': credentials_path}
 
     logger.info('Telegram-бот: Запуск...')
     updater = Updater(telegram_token, use_context=True)
-
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
-    dispatcher.add_error_handler(error_handler)
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command,
+                       partial(handle_message, dialogflow_settings=dialogflow_settings,
+                               notifier_settings=notifier_settings)))
+    dispatcher.add_error_handler(partial(error_handler, notifier_settings=notifier_settings))
 
     updater.start_polling()
     logger.info('Telegram-бот запущен и получает обновления.')
